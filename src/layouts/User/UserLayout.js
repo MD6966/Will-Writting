1cf015bd8827b90902e42469c60a9d93795ebf42
Syed Mudasser Anayat <107185992+MD6966@users.noreply.github.com>
2023-08-25 17:25:18 +0500

    changes

--- src/layouts/User/UserLayout.js
+++ src/layouts/User/UserLayout.js
@@ -1,7 +1,40 @@
 import React from 'react'
-import { AppBar, Avatar, Box, Button, Toolbar, Typography } from '@mui/material'
+import { AppBar, Avatar, Box, Button, Menu, 
+  MenuItem, Toolbar, Typography, Container,
+Divider,
+List,
+ListItem,
+ListItemIcon,
+ListItemText,
+Dialog,
+DialogTitle,
+DialogContent,
+Card,
+} from '@mui/material'
+import Settings from '@mui/icons-material/Settings';
 import { Link, Outlet } from 'react-router-dom'
+import PersonIcon from '@mui/icons-material/Person';
+import Logout from '@mui/icons-material/Logout';
+
 const UserLayout = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const [dOpen, setDopen] = React.useState(false)
+  const handleAvatarClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  }
+  const handleClose = () => {
+    setAnchorEl(null);
+
+  }
+
+  const handleOpenD = () => {
+    setDopen(true)
+    setAnchorEl(null)
+  }
+  const handleDclose = () => {
+    setDopen(false)
+  }
   return (
     <div>
       <AppBar position='static'>
@@ -19,12 +52,56 @@ const UserLayout = () => {
                 sx={{ml:2}}> Complete Form</Button>
                 </Box>
                 <Box sx={{ml:2}}>
-                    <Avatar src="/assets/images/user.png" sx={{background:'#e2e2e2'}} />
+                    <Avatar src="/assets/images/user.png" 
+                    sx={{background:'#e2e2e2', cursor:'pointer'}}
+                    onClick={handleAvatarClick}
+                    />
                 </Box>
 
             </Box>
         </Toolbar>
       </AppBar>
+      <Menu
+      open={open}
+      anchorEl={anchorEl}
+      onClose={handleClose}
+      >
+        
+        <Container>
+          <Typography variant='h6' style={{fontWeight:'bold'}}> User Name </Typography>
+          </Container>
+          <Divider style={{margin:'0px 10px'}} /> 
+          <MenuItem onClick={handleOpenD}>
+          <PersonIcon /> <Typography sx={{ml:1}}> View Profile </Typography>
+          </MenuItem>
+          <Divider /> 
+          <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+      <Dialog open={dOpen} fullWidth onClose={handleDclose}>
+        <DialogTitle >
+         <PersonIcon sx={{verticalAlign:'bottom', fontSize:'2rem'}} />  View Profile 
+        </DialogTitle>
+        <Divider />
+        <DialogContent>
+          <Card elevation={2} sx={{p:2}}>
+            <Box sx={{display:'flex', justifyContent:'center'}}>
+            <Avatar src="/assets/images/user.png" sx={{height:'50px', background:'#F2f2f2'}} />
+            <Typography sx={{mt:1.5, ml:2}} variant='h5'> User Name</Typography>
+            </Box>
+          </Card>
+        </DialogContent>
+      </Dialog>
       <Outlet /> 
     </div>
   )
